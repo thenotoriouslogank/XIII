6720c259467e19ef23746a3409b3232cd8aacb27
logank@orion <logan.aker1989@gmail.com>
2022-04-09 20:17:00 -0400

    Created systemd service file

--- XIII/bot/extensions/version.py
+++ XIII/bot/extensions/version.py
@@ -3,13 +3,12 @@ from XIII.bot import Bot
 from XIII import __version__
 
 plugin = lightbulb.Plugin("version")
-version = __version__
 
 @plugin.command
 @lightbulb.command("version", "Returns bot version information.")
 @lightbulb.implements(lightbulb.PrefixCommand)
-async def ping(ctx: lightbulb.Context) -> None:
-    await ctx.respond(f"{version}")
+async def version(ctx: lightbulb.Context) -> None:
+    await ctx.respond(f"{__version__}")
 
 def load(bot: Bot) -> None:
     bot.add_plugin(plugin)
